bc79042686a81cff9d7de0fad620a6d0c564d64b
dsmedium <46603548+dsmedium@users.noreply.github.com>
2019-12-02 03:11:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const sendMail = require('./mail.js')
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
 
 // Data Parsing
